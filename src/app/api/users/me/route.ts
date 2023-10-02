7f6e6bbe12687dfee7c82d578e8f6e71b5b2b58b
pawan <pawannane23@gmail.com>
2023-10-02 11:50:44 +0530

    return response from me handler

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,13 +8,13 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDataFromToken(request);
-    const user = await User.findOne({_id: userId}).select("-password");
+    const user = await User.findOne({ _id: userId }).select("-password");
 
     return NextResponse.json({
       message: "User found",
       data: user
     })
   } catch (error: any) {
-    NextResponse.json({ error: error.message}, {status: 400})
+    return NextResponse.json({ error: error.message }, { status: 400 })
   }
 }
